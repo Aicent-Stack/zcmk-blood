a1b96bf2a588cd02dcce07b2cbfa11285002ba55
Aicent Stack <lee@aicent.com>
2026-04-03 23:25:57 +0800

    Update circulatory.rs

--- src/circulatory.rs
+++ src/circulatory.rs
@@ -1,7 +1,7 @@
 // Aicent Stack | ZCMK (Zero-Commission Marketplace & Knot)
 // Domain: http://zcmk.com
+//! Specification: RFC-004 Standard (Active).
 // Purpose: Nanosecond resource circulation & 0.00% commission auctions.
-// Specification: RFC-004 Draft.
 // License: Apache-2.0 via Aicent.com Organization.
 //! # RFC-004: ZCMK Value Circulation
 
